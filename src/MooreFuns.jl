7b03a8a1421956d1cb10147c1b0a229d296b9d72
rvcrawford <rvcrawford@gmail.com>
2023-09-07 21:56:23 -0400

    add ccb dict, struct for accessions

--- src/MooreFuns.jl
+++ src/MooreFuns.jl
@@ -2,12 +2,10 @@ module MooreFuns
 
 # Write your package code here.
 export greet_your_package_name
+export accession_template
+export ccb_dict
 include("functions.jl")
 
-function hello_world()
-    println("hello world")
-end
 
-export hello_world
 
 end
--- src/functions.jl
+++ src/functions.jl
@@ -2,4 +2,15 @@ function greet_your_package_name()
     println("Hello MooreFuns")
 end
 
+struct accession_template
+    accession::String
+    species::String
+end
+
+ccb_dict = Dict{String,String}("wp"=> "Pisum sativum", 
+"hv" => "Vicia villosa",
+"cc" => "Trifolium incarnatum",
+"cr" => "Secale cereale"
+)
+
 # export greet_your_package_name
