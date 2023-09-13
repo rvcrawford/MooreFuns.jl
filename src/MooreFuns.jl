10fdcdc113db78c030888c3148a413518b5a0615
rvcrawford <rvcrawford@gmail.com>
2023-09-12 21:48:39 -0400

    add some new functions, helper objects to make ccb objects

--- src/MooreFuns.jl
+++ src/MooreFuns.jl
@@ -10,6 +10,9 @@ export test_df
 export brassica_df
 export bb_col_names_df
 export accession_df
+export make_grid
+export ccb_trial_header
+export make_ccb_missings
 include("functions.jl")
 
 end
--- src/functions.jl
+++ src/functions.jl
@@ -22,12 +22,35 @@ function make_accession(accession::String, species_abb::String)
     accession_template(accession, species)
 end
 
+# makes a dataframe including the
 function accession_df(x::accession_template)
     df = DataFrames.DataFrame(x)
     to_add = DataFrames.DataFrame(population_name = missing, organization_name = missing, synonym = missing)
     to_add2 = repeat(to_add, DataFrames.nrow(df))
     hcat(df, to_add2)
 end
+
+
+function make_grid(n_row::Int, n_col::Int)
+    row_df = DataFrames.DataFrame(row_number = 1:n_row)
+    col_df = DataFrames.DataFrame(col_number = 1:n_col)
+    cross_df = DataFrames.crossjoin(row_df, col_df)
+    cross_df.plot_number = cross_df.row_number .* 100 .+ cross_df.col_number
+end
+
+
+ccb_trial_header = DataFrames.DataFrame(trial_name = String[],
+breeding_program = String[],
+location = String[],
+year = Int64[],
+design_type = String[],
+description = String[]
+)
+
+
+
+    # cross_df.accession_name = accession_name
+    # cross_df.block_number = repeat(1:4,inner = 15)
 
 # define a path to our data directory
 datapath = joinpath(@__DIR__, "..", "data")
